a758086b38597eb388a0338e17ef664fe33727a3
mcpeutk <mcpeutk@gmail.com>
2020-03-31 11:25:43 +0300

    Обновлена функция обработки строки

--- src/string_parser.py
+++ src/string_parser.py
@@ -8,10 +8,15 @@ punctuation = punctuation.replace('@', '') + '\n'
 tt = str.maketrans(dict.fromkeys(punctuation))
 
 def prepareString(text):
-    tokens = text.lower().split(' ')
-    tok = []
-    for token in tokens:
-        t = token.translate(tt)
-        if t not in russian_stopwords and t != ' ' and t != '':
-            tok.append(t)
-    return ' '.join(tok)
+    res_text = []
+    text = text.lower().split()
+
+    for word in text:
+        res_str = ""
+        for i in range(len(word)):
+            if (word[i] >= '0' and word[i] <= '9') or (word[i] >= 'a' and word[i] <= 'z'):
+                res_str += word[i]
+        if (res_str != ''):
+            res_text.append(res_str)
+            
+    return ' '.join(res_text)
