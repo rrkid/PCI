c6d915ff01b60db3dcb6cd225572b23ca6f603d0
mmlofficial <47824135+mmlofficial@users.noreply.github.com>
2020-04-02 15:05:32 +0300

    Устранение неполадок

--- src/data_preparer_pro.py
+++ src/data_preparer_pro.py
@@ -8,8 +8,8 @@ import string_parser_pro
 df_train = pd.read_csv('../tmp/data/train.csv')
 df_val = pd.read_csv('../tmp/data/val.csv')
 
-df_val.description = df_val.description.apply(string_parser.prepareString)
-df_train.description = df_train.description.apply(string_parser.prepareString)
+df_val.description = df_val.description.apply(string_parser_pro.prepareString)
+df_train.description = df_train.description.apply(string_parser_pro.prepareString)
 
-df_val.to_csv('../tmp/prepared_data/prepared_val.csv', index = False)
-df_train.to_csv('../tmp/prepared_data/prepared_train.csv', index = False)
+df_val.to_csv('../tmp/prepared_data/prepared_val_pro.csv', index = False)
+df_train.to_csv('../tmp/prepared_data/prepared_train_pro.csv', index = False)
