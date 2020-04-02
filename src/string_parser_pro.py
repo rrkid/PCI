2265e3cde4fdb75f4abb9d65b74f69ebd7cac890
mmlofficial <47824135+mmlofficial@users.noreply.github.com>
2020-04-02 16:29:53 +0300

    Изменен способ обработки строки

--- src/string_parser_pro.py
+++ src/string_parser_pro.py
@@ -1,30 +1,28 @@
-import nltk
-nltk.download("stopwords")
-from nltk.corpus import stopwords
-from string import punctuation
-
-russian_stopwords = stopwords.words("russian")
-russian_stopwords[:] = ['' if x in ['один', 'два', 'три'] else x for x in russian_stopwords]
-punctuation = punctuation.replace('@', '') + '\n'
-tt = str.maketrans(dict.fromkeys(punctuation))
+import re
 
 digitsDict = {'ноль': 0, 'один': 1, 'два': 2, 'три': 3, 'четыре': 4, 'пять': 5, 'шесть': 6, 'семь': 7, 'восемь': 8, 'девять': 9}
 
 def replaceDigit(value):
     return value if digitsDict.get(value) == None else str(digitsDict.get(value))
 
+def deleteJunk(word):
+    reg = re.compile('[^a-zA-Z0-9а-яА-Я@.+-]')
+    return reg.sub('', word)
+    
+def deleteRussian(word):
+    reg = re.compile('[^a-zA-Z0-9@.+-]')
+    return reg.sub('', word)
+
 def prepareString(text):
     tokens = text.lower().split(' ')
     tok = []
     for token in tokens:
-        t = token.translate(tt)
-        if t not in russian_stopwords and t != ' ' and t != '':
-            t = replaceDigit(t)
+        t = deleteJunk(token)
+        t = replaceDigit(t)
+        t = deleteRussian(t)
+        if t != ' ' and t != '':
             tok.append(t)
     return ' '.join(tok)
 
-
 if __name__ == '__main__':
     pass
-
-
