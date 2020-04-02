2a507649941abd2769ff1bd9255f63e76e6a066b
mmlofficial <47824135+mmlofficial@users.noreply.github.com>
2020-04-02 18:00:28 +0300

    Файлы обработаны

--- src/data_preparer_pro.py
+++ src/data_preparer_pro.py
@@ -5,11 +5,11 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../src")
 
 import string_parser_pro
 
-df_train = pd.read_csv('../tmp/data/train.csv')
-df_val = pd.read_csv('../tmp/data/val.csv')
 
+df_val = pd.read_csv("../tmp/data/val.csv")
 df_val.description = df_val.description.apply(string_parser_pro.prepareString)
-df_train.description = df_train.description.apply(string_parser_pro.prepareString)
+df_val.to_csv("../tmp/prepared_data_pro/prepared_val_pro.csv", index = False)
 
-df_val.to_csv('../tmp/prepared_data_pro/prepared_val_pro.csv', index = False)
-df_train.to_csv('../tmp/prepared_data_pro/prepared_train_pro.csv', index = False)
+df_train = pd.read_csv("../tmp/data/train.csv")
+df_train.description = df_train.description.apply(string_parser_pro.prepareString)
+df_train.to_csv("../tmp/prepared_data_pro/prepared_train_pro.csv", index = False)
